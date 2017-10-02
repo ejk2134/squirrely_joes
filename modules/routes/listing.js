e0dab17f1f173086d3537d7f23a191de4ecddf5e
Evan Kearney <evanjkearney@gmail.com>
2017-10-01 21:53:12 -0500

    Add post route

--- modules/routes/listing.js
+++ modules/routes/listing.js
@@ -18,5 +18,32 @@ router.get('/', function(req, res){
     })
 })
 
+router.post('/', function(req, res){
+    console.log(req.body);
+    if (req.body.type === 'rent'){
+        var newListing = {
+            rent: req.body.rent,
+            sqft: req.body.sqft,
+            city: req.body.city
+        }
+    }else{
+        var inputs = {
+            cost: req.body.cost,
+            sqft: req.body.sqft,
+            city: req.body.city
+        }
+    }
+    
+    var newListing = Listing(inputs);
+    newListing.save(function(error){
+        if (error){
+            console.log(error);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(201);
+        }
+    })
+})
+
 
 module.exports = router;
